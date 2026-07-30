b325e50d8ec9eab8f8536712f86aafc6de2fdefb
andrespatinoavila-droid <andrespatinoavila@gmail.com>
2026-07-30 07:18:55 -0500

    Publicar Wedding Experience v1.1.1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import PhotoSwipe from "../vendor/photoswipe/photoswipe.esm.min.js";
 import {
   detectExperienceConfig,
-} from "./experience-config.js?v=1.1.0";
+} from "./experience-config.js?v=1.1.1";
 
 const EXPERIENCE_CONFIG = detectExperienceConfig();
 document.documentElement.classList.add(EXPERIENCE_CONFIG.className);
