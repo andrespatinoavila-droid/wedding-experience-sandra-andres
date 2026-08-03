7632229df476a6a8e39a55e17b445047450d37a7
andrespatinoavila-droid <andrespatinoavila@gmail.com>
2026-08-02 20:04:44 -0500

    preview: publish onion soup RC 800f0b3

---
+++ rc-html-sopa-800f0b3/menu-html/menu-data.js
@@ -0,0 +1,118 @@
+export const menuSections = [
+  {
+    id: "entradas",
+    title: "Entradas",
+    items: [
+      {
+        id: "sopa-cebolla",
+        name: "Sopa de Cebolla Gratinada",
+        description:
+          "Cebollas en reducción de vino tinto y salsa de carne, sobre una tostada con queso gratinado.",
+        icon: "cup",
+        image: "img/dishes/sopa-cebolla-transparente.png",
+      },
+      {
+        id: "tartar-salmon",
+        name: "Tartar de Salmón sobre Rosti",
+        description:
+          "Rodajas de salmón salteadas en sal y pimienta con alcaparras sobre nuestra tradicional papa rosti suiza.",
+        icon: "fish",
+      },
+      {
+        id: "tartiflette",
+        name: "Tartiflette",
+        description:
+          "Plato franco-suizo: patatas cortadas en rodajas finas con trozos de tocineta y nuestro queso cremoso suizo, gratinado.",
+        icon: "cheese",
+        image: "img/dishes/tartiflette-transparente.png",
+      },
+    ],
+  },
+  {
+    id: "platos-fuertes",
+    title: "Platos fuertes",
+    items: [
+      {
+        id: "filet-mignon",
+        name: "Filet Mignon",
+        description:
+          "Medallones de res a la parrilla, recubiertos con tocineta y salteados en salsa de champiñones a base de vino tinto.",
+        icon: "cow",
+      },
+      {
+        id: "costillas",
+        name: "Costillas de Cerdo en Flor de Jamaica",
+        description:
+          "Rack de costillas de cerdo al horno de leña, con salsa a elección: BBQ, mandarina o teriyaki.",
+        icon: "pig",
+      },
+      {
+        id: "new-york",
+        name: "New York Steak",
+        description:
+          "320 gramos de lomo de res, en corte firme y marmoleado, caracterizado por su jugosidad y ternura.",
+        icon: "cow",
+      },
+      {
+        id: "pina-pomodoro",
+        name: "Piña Rellena al Pomodoro",
+        description:
+          "Media piña rellena de julianas de pollo y camarones salteados en salsa pomodoro y quesos suizos.",
+        icon: "pineapple",
+      },
+      {
+        id: "lombarda-pollo",
+        name: "Lombarda de Pollo",
+        description:
+          "Pollo a la parrilla salteado en salsa con reducción de vino tinto suizo, tocineta y champiñones.",
+        icon: "chicken",
+      },
+      {
+        id: "arroz-waldorf",
+        name: "Arroz Waldorf",
+        description:
+          "Arroz blanco con almejas, mejillones, róbalo, camarones, salmón y corvina, acompañado de hojuelas de plátano.",
+        icon: "shell",
+      },
+      {
+        id: "salmon-wolford",
+        name: "Salmón Wolford",
+        description:
+          "Filete de salmón a la parrilla con frutos del mar y salsa marinera de la casa.",
+        icon: "fish",
+      },
+      {
+        id: "corvina-bretona",
+        name: "Corvina Bretona",
+        description:
+          "Filete de corvina a la parrilla en salsa al ajillo suave con camarón y champiñones flambeados.",
+        icon: "fish",
+      },
+    ],
+  },
+  {
+    id: "acompanamientos",
+    title: "Acompañamientos",
+    items: [
+      {
+        id: "pure-papa",
+        name: "Puré de Papa",
+        description: "Suave puré de papa cremoso y mantequilloso.",
+        icon: "side",
+      },
+      {
+        id: "ensalada",
+        name: "Ensalada Fresca",
+        description: "Mezcla de lechugas frescas con vegetales de temporada.",
+        icon: "side",
+      },
+      {
+        id: "arroz-almendra",
+        name: "Arroz de Almendra",
+        description:
+          "Arroz aromático salteado con almendras tostadas y un toque de mantequilla.",
+        icon: "side",
+      },
+    ],
+  },
+];
